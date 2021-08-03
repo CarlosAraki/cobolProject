e0b97efdb15e80eaecd0e4c406031f2be5f74479
CarlosAraki <carlosarak@gmail.com>
2021-08-03 20:35:26 +0000

    fix : its not necessary cobol compiler

--- hercules-3.13/build_pch.c
+++
@@ -1,4 +0,0 @@
-/* BUILD_PCH  (c)Copyright Ivan Warren, 2005-2009                    */
-/*            Dummy module for building pre-compiled header files    */
-
-#include "hstdinc.h"
--- hercules-3.13/fillfnam.h
+++
@@ -1,9 +0,0 @@
-/* FILLFNAM.H   (c) Copyright Volker Bandke, 2003-2006               */
-/*              Hercules filename completion functions               */
-
-#ifndef __FILLFNAM_H__
-#define __FILLFNAM_H__
-
-int tab_pressed(char *cmdlinefull, int *cmdoffset);
-
-#endif
